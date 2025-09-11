edfe316ccf371a6197dd639a80745daeccab693f
Yoshicaballo <jaydrien.cruz-rivera@innovationcharter.org>
2025-09-11 18:40:07 +0000

    J/R Homer and abe added yay

--- main.js
+++ main.js
@@ -6,4 +6,14 @@ let app = document.querySelector("#app");
 let ti = new TextInterface(app, "Example Text Interface");
 let name = await ti.prompt("What is your name?");
 ti.output("Hello, " + name);
+let gender = await ti.promptYesOrNo("Are you a boy?");
+if (gender === true) {
+  let work = await ti.promptYesOrNo("Do you have a j*b?");
+  if (work === true) {
+    ti.output("HOMER!")
+    
+  } else 
+    {ti.output("ABE!")
 
+  }
+} 

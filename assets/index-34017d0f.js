19df6475ca2d9411fff878798fca588b2b6c244b
Yoshicaballo <Yoshicaballo@users.noreply.github.com>
2025-09-11 18:40:25 +0000

    deploy: edfe316ccf371a6197dd639a80745daeccab693f

--- assets/index-34017d0f.js
+++ assets/index-34017d0f.js
@@ -200,3 +200,14 @@ let app = document.querySelector("#app");
 let ti = new a(app, "Example Text Interface");
 let name = await ti.prompt("What is your name?");
 ti.output("Hello, " + name);
+let gender = await ti.promptYesOrNo("Are you a boy?");
+if (gender === true) {
+  let work = await ti.promptYesOrNo("Do you have a j*b?");
+  if (work === true) {
+    ti.output("HOMER!");
+    
+  } else 
+    {ti.output("ABE!");
+
+  }
+}

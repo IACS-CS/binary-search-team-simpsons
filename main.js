8e2383e5d72dfad6ce5c9ecbaf074a725cf968e9
Wallrisss <Robbyrev7@gmail.com>
2025-09-22 13:19:22 +0000

    finished

--- main.js
+++ main.js
@@ -3,47 +3,91 @@ import { TextInterface } from "text-interface";
 
 let app = document.querySelector("#app");
 // Create a new "Text Interface"
+let playing = true;
 let ti = new TextInterface(app, "Think of Simpson's family character");
-let name = await ti.prompt("What is your name?");
-ti.output("Hello, " + name);
-let gender = await ti.promptYesOrNo("Are you a boy?");
-if (gender === true) {
-  let work = await ti.promptYesOrNo("Do you have a j*b?");
-  if (work === true) {
-    ti.output("HOMER!");
-  } else {
-    let skateboard = await ti.promptYesOrNo("Do you like skateboarding?");
-    if (skateboard === true) {
-      ti.output("BART!")
-      ti.showImage("Bart_Simpson_200px.png");
+while (playing) {
+  let name = await ti.prompt("What is your name?");
+  ti.output("Hello, " + name);
+  let gender = await ti.promptYesOrNo("Are you a boy?");
+  if (gender === true) {
+    //gender
+    let work = await ti.promptYesOrNo("Do you have a j*b?");
+    if (work === true) {
+      //if true homer if false keep going
+      ti.output("HOMER!");
+      ti.showImage("Homer_Simpson_2006.png");
     } else {
-      let Old = await ti.promptYesOrNo("Do you have back problems?");
-      if (Old === true) {
-        ti.output("ABE!");
+      let skateboard = await ti.promptYesOrNo("Do you like skateboarding?");
+      if (skateboard === true) {
+        //bart
+        ti.output("BART!");
+        ti.showImage("Bart_Simpson_200px.png");
       } else {
-        ti.output("SANTA'S LITTLE HELPER!");
+        let Old = await ti.promptYesOrNo("Do you have back problems?");
+        if (Old === true) {
+          //abe
+          ti.output("ABE!");
+          ti.showImage("Abe_Simpson.png");
+        } else {
+          ti.output("SANTA'S LITTLE HELPER!");
+          ti.showImage("SantasLittleHelper.png");
+        }
+      }
+    }
+  } else {
+    let Shopping = await ti.promptYesOrNo("Do you go shopping?");
+    if (Shopping === true) {
+      //shoping
+      let Adult = await ti.promptYesOrNo("Are you an adult?");
+      if (Adult === true) {
+        //adult marge or maggie
+        ti.output("MARGE!");
+        TimeRanges.showImage("Marge_Simpson.png");
+      } else {
+        ti.output("MAGGIE!");
+        ti.showImage("Maggie_Simpson.png");
+      }
+    } else {
+      let Saxophone = await ti.promptYesOrNo("Do you play the saxophone?");
+      if (Saxophone === true) {
+        //sax
+        ti.output("LISA!");
+        ti.showImage("Lisa_Simpson.png");
+      } else {
+        let living = await ti.promptYesOrNo("Are you alive?");
+        if (living === true) {
+          //alive
+          ti.output("SNOWBALL 2!");
+        } else {
+          ti.output("SNOWBALL 1!");
+          ti.showImage("Snowball_I_Tapped_Out.webp");
+          ti.showImage("30x15_Snowball.webp");
+        }
       }
     }
   }
-} else {
-  let Shopping = await ti.promptYesOrNo("Do you go shopping?");
-  if (Shopping === true) {
-    let Adult = await ti.promptYesOrNo("Are you an adult?");
-    if (Adult === true) {
-      ti.output("MARGE!");
-    } else {
-      ti.output("MAGGIE!");
+
+  let final = await ti.promptYesOrNo("did i get it right");
+  if (final === true) {
+    //final
+    let restart = await ti.promptYesOrNo("do you want to play again");
+    {
+      if (restart === true) {
+        //play again
+        playing === false;
+      } else {
+        ti.output("bye then");
+        playing = false
+      }
     }
   } else {
-    let Saxophone = await ti.promptYesOrNo("Do you play the saxophone?");
-    if (Saxophone === true) {
-      ti.output("LISA!");
-    } else {
-      let living = await ti.promptYesOrNo("Are you alive?");
-      if (living === true) {
-      ti.output("SNOWBALL 1!");
+    ti.output("LIAR");
+    let restart = await ti.promptYesOrNo("do you want to play again");
+    {
+      if (restart === true) {
       } else {
-        ti.output("SNOWBALL 2!")
+         ti.output("bye then");
+         playing = false
       }
     }
   }
